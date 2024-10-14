5c815444a65f195805335638b6121659fd227282
Dwarz <Dwarz@users.noreply.github.com>
2024-10-14 14:32:37 +0200

    Added existense of board (empty) and Player
    Player contains caravan, hand, playedcards.

---
+++ player.py
@@ -0,0 +1,6 @@
+class Player:
+    caravan = [1,1,1] #creates caravan with start resources
+    hand = [] #TODO
+    playedcards = [] #TODO
+
+    #first player starts with 3x lowest spice in caravan, 
--- setup.py
+++ setup.py
@@ -1,2 +1,25 @@
 #something something
-print ("Hello")
+print ("Hello") # py .\setup.py to run it
+
+import pygame 
+import board
+import player
+  
+pygame.init() 
+  
+# CREATING CANVAS 
+canvas = pygame.display.set_mode((1000, 1000)) 
+clock = pygame.time.Clock()
+  
+# TITLE OF CANVAS 
+pygame.display.set_caption("My Board") 
+running = True
+  
+while running: 
+    for event in pygame.event.get(): 
+        if event.type == pygame.QUIT: 
+            running = False
+    clock.tick(60)  # limits FPS to 60
+
+    pygame.draw.rect(canvas, (255,0,0), pygame.Rect(30,30,60,60)) 
+    pygame.display.update() 
